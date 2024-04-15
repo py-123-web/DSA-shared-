4e456c84f633bb53219b7a88b8811444f8c92ed8
NinjazTurtle <166167985+NinjazTurtle@users.noreply.github.com>
2024-04-15 21:02:40 +0800

    Update AssignmentTeam.java

--- entity/AssignmentTeam.java
+++ entity/AssignmentTeam.java
@@ -4,53 +4,63 @@ ASSIGNMENT TEAM MANAGEMENT SUBSYSTEM
  */
 package entity;
 
-import java.io.Serializable;
-import java.util.Date;
+import adt.SortedArrayList;
+import adt.SortedListInterface;
 
-public class AssignmentTeam implements Serializable{
-    private static int tID = 0;
+
+public class AssignmentTeam implements Comparable <AssignmentTeam>{
+    
+    private String tutorialGrp;//(G1)
+    public String getTutorialGrp() {
+        return tutorialGrp;
+    }
+    public void setTutorialGrp(String tutorialGrp) {
+        this.tutorialGrp = tutorialGrp;
+    }
+    
+    
+    
+    private SortedListInterface<Course> courseList;//courseCode=(DSA)
+    private SortedListInterface<Programme> programmeList;//programmeCode=REI
+    private SortedListInterface<TutorialGrp> tutorialGroupList;//(G1), Programme=(REI)
+    private Course course;
+    private Programme programme;
+    //private TutorialGrp tutorialGrp;
+    
     private String teamId;
-    private String teamLeader;
+    private int maxNumOfStud;
+    private int maxNumOfTeam;
+    private int numOfTeamToCreate;
+    //private Course course;
+    //private TutorialGroup;
+    //private Programme programme;
     
-    
-    //private Course course
-    //   -get courseId(DSA),  programme(REI(no fullname)
-    //  - xuyao tambah year
-    private String courseId; 
-    private String programme;
-    private int year;
-    private int Semester;
-    //private TutorialGroup
-    //  - get tutorialGroupID
-    private String tutorialGroupID;
-    //private Tutor tutor;    
-    //  - get tutorId
-    private String tutorId;
+    //TeamMember
     //private Student student;
-    //  - get studId, studName, email
-    private String studId;
-    //need to calculate the year, semester
+    private String teamLeader;
+    private String studId; //  - get studId, studName, email
+
+    public SortedListInterface<Course> getCourseList() {
+        return courseList;
+    }
+    public SortedListInterface<TutorialGrp> getTutorialGroupList() {
+        return tutorialGroupList;
+    }
+    public SortedListInterface<Programme> getProgrammeList() {
+        return programmeList;
+    }
+    
+    public Course getCourse() {
+        return course;
+    }
+    public Programme getProgramme() {
+        return programme;
+    }
+    
+
     
     
-    public AssignmentTeam(){
-        
-    }
-
-    public static int gettID() {
-        return tID;
-    }
-
-    public static void settID(int tID) {
-        AssignmentTeam.tID = tID;
-    }
-
-    public String getTeamId() {
-        return teamId;
-    }
-
-    public void setTeamId(String teamId) {
-        this.teamId = teamId;
-    }
+    
 
     public String getTeamLeader() {
         return teamLeader;
@@ -60,54 +70,6 @@ public class AssignmentTeam implements Serializable{
         this.teamLeader = teamLeader;
     }
 
-    public String getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(String courseId) {
-        this.courseId = courseId;
-    }
-
-    public String getProgramme() {
-        return programme;
-    }
-
-    public void setProgramme(String programme) {
-        this.programme = programme;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public int getSemester() {
-        return Semester;
-    }
-
-    public void setSemester(int Semester) {
-        this.Semester = Semester;
-    }
-
-    public String getTutorialGroupID() {
-        return tutorialGroupID;
-    }
-
-    public void setTutorialGroupID(String tutorialGroupID) {
-        this.tutorialGroupID = tutorialGroupID;
-    }
-
-    public String getTutorId() {
-        return tutorId;
-    }
-
-    public void setTutorId(String tutorId) {
-        this.tutorId = tutorId;
-    }
-
     public String getStudId() {
         return studId;
     }
@@ -115,7 +77,87 @@ public class AssignmentTeam implements Serializable{
     public void setStudId(String studId) {
         this.studId = studId;
     }
+
     
+    
+    
+    
+   
+
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(String teamId) {
+        this.teamId = teamId;
+    }
+
+    public int getMaxNumOfStud() {
+        return maxNumOfStud;
+    }
+
+    public void setMaxNumOfStud(int maxNumOfStud) {
+        this.maxNumOfStud = maxNumOfStud;
+    }
+
+    public int getMaxNumOfTeam() {
+        return maxNumOfTeam;
+    }
+
+    public void setMaxNumOfTeam(int maxNumOfTeam) {
+        this.maxNumOfTeam = maxNumOfTeam;
+    }
+
+    public int getNumOfTeamToCreate() {
+        return numOfTeamToCreate;
+    }
+
+    public void setNumOfTeamToCreate(int numOfTeamToCreate) {
+        this.numOfTeamToCreate = numOfTeamToCreate;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public AssignmentTeam(){
+        
+    }
+    
+    //create
+    public AssignmentTeam(String teamId, int maxNumOfStud, Course course, Programme programme, String tutorialGrp) {
+        this.tutorialGrp = tutorialGrp;
+        this.course = course;
+        this.programme = programme;
+        this.teamId = teamId;
+        this.maxNumOfStud = maxNumOfStud;
+    }
+    
+    
+
+    
+    //input detail
+    public AssignmentTeam(Course course, Programme programme, String tutorialGrp,  int maxNumOfStud, int numOfTeamToCreate) {
+        this.tutorialGrp = tutorialGrp;
+        this.course = course;
+        this.programme = programme;
+        this.maxNumOfStud = maxNumOfStud;
+        this.numOfTeamToCreate = numOfTeamToCreate;
+    }
+    
+    
+    
+    @Override
+    public int compareTo(AssignmentTeam o) {
+    return teamId.compareTo(o.teamId);
+  }
+    
+
     
    
 
