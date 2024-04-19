529e434f9897a6cf5a8f90d78a5a6490de5b28f7
lwy09 <166479040+lwy09@users.noreply.github.com>
2024-04-19 22:05:33 +0800

    Update RegisterCourse.java

--- entity/RegisterCourse.java
+++ entity/RegisterCourse.java
@@ -1,8 +1,6 @@
 package entity;
 
 import adt.*;
-import java.util.Date;
-import java.util.Objects;
 
 /**
  *
@@ -10,47 +8,25 @@ import java.util.Objects;
  */
 public class RegisterCourse implements Comparable<RegisterCourse> {
 
-    private int registerId;
-    private double amount;
-    // private Date registerDate;
-    private SortedListInterface<Course> courseList = new SortedArrayList<>();// one Register Course, many Courses
+    private SortedListInterface<Student> studentList = new SortedArrayList<>();
+    private SortedListInterface<Course> courseList = new SortedArrayList<>();
+    private double feesPaid;
 
     public RegisterCourse() {
     }
 
-    public RegisterCourse(int registerId, double amount) {
-        this.registerId = registerId;
-        this.amount = amount;
-    }
-//        public RegisterCourse(int registerId, double amount, Date registerDate) {
-//        this.registerId = registerId;
-//        this.amount = amount;
-//        this.registerDate = registerDate;
-//    }
-
-    public int getRegisterId() {
-        return registerId;
+    public RegisterCourse(double feesPaid) {
+        this.feesPaid = feesPaid;
     }
 
-    public void setRegisterId(int registerId) {
-        this.registerId = registerId;
+    public SortedListInterface<Student> getStudentList() {
+        return studentList;
     }
 
-    public double getAmount() {
-        return amount;
+    public void setStudentList(SortedListInterface<Student> studentList) {
+        this.studentList = studentList;
     }
 
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-//    public Date getRegisterDate() {
-//        return registerDate;
-//    }
-//
-//    public void setRegisterDate(Date registerDate) {
-//        this.registerDate = registerDate;
-//    }
     public SortedListInterface<Course> getCourseList() {
         return courseList;
     }
@@ -59,33 +35,22 @@ public class RegisterCourse implements Comparable<RegisterCourse> {
         this.courseList = courseList;
     }
 
+    public double getFeesPaid() {
+        return feesPaid;
+    }
+
+    public void setFeesPaid(double feesPaid) {
+        this.feesPaid = feesPaid;
+    }
+
     @Override
     public String toString() {
-        return "%-15s %-10s\n"
-                + registerId + amount;
+        return String.format("%-8.2f\n", feesPaid);
     }
-    //    @Override
-//    public String toString() {
-//        return "%-15s %-10s %-15s\n"
-//                + registerId + amount + registerDate;
-//    }
 
     @Override
     public int compareTo(RegisterCourse otherRegisterCourse) {
-        // Compare based on registerId or any other attribute that defines the natural order
-        return Integer.compare(this.registerId, otherRegisterCourse.registerId); // sort based on registerId
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        //this method is specifically designed to check if two RegisterCourse objects are equal by comparing their register IDs(registerId)
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        RegisterCourse registerCourse = (RegisterCourse) o;
-        return Objects.equals(registerId, registerCourse.registerId);
+        // Compare based on feesPaid or any other attribute that defines the natural order
+        return Double.compare(this.feesPaid, otherRegisterCourse.feesPaid); // sort based on feesPaid
     }
 }
