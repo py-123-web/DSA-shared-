06ddd9724be7b64d6716357e029d483867a5147c
lwy09 <166479040+lwy09@users.noreply.github.com>
2024-04-21 15:48:18 +0800

    Update StudentRegistrationUI.java

--- boundary/StudentRegistrationUI.java
+++ boundary/StudentRegistrationUI.java
@@ -1,11 +1,11 @@
 package boundary;
 
-import entity.Student;
 import java.util.Scanner;
+import utility.DateTime;
 
 /**
  *
- * @author LWY
+ * @author Lee Weng Yan
  */
 public class StudentRegistrationUI {
 
@@ -13,17 +13,17 @@ public class StudentRegistrationUI {
 
     public int getMenuChoice() {
         System.out.println("\nSTUDENT REGISTRARION MANAGEMENT SUBSYSTEM");
-        System.out.println(  "=========================================");
+        System.out.println("=========================================");
         System.out.println("1. List all students");
         System.out.println("2. Add new students");
         System.out.println("3. Remove a student");
         System.out.println("4. Amend student details");
         System.out.println("5. Search students for registered courses");
-        System.out.println("6. Add students to a few courses (main,elective,resit,repeat)");
-        System.out.println("7. Remove a student from a course (main,elective) registration");
-        System.out.println("8. Calculate fee paid for registered courses");
-        System.out.println("9. Filters students for courses based on citeria");
-        System.out.println("10. Generate summary reports x2");
+        System.out.println("6. Add students to a few courses & Calculate fee paid");
+        System.out.println("7. Remove a student from a course registration");
+        System.out.println("8. Filters students for courses based on citeria");
+        System.out.println("9. Student Age Analysis Summary Report");
+        System.out.println("10. Generate [change name in UI] summary reports");
         System.out.println("0. Quit");
         System.out.print("Enter choice: ");
         int choice = scanner.nextInt();
@@ -32,94 +32,105 @@ public class StudentRegistrationUI {
         return choice;
     }
 
-    
-
-
-//    public void listAllStudents(String outputStr) {
-//        System.out.println("\nList of Students:\n" + outputStr);
-//    }
-//
-//    public void printStudentDetails(Student student) {
-//        System.out.println("\nStudent Details");
-//        System.out.println(  "===============");
-//        System.out.println("Student ID:" + student.getStudId());
-//        System.out.println("Student Name: " + student.getStudName());
-//        System.out.println("IC: " + student.getIc());
-//        System.out.println("Gender: " + student.getGender());
-//        System.out.println("Contact No: " + student.getContactNo());
-//        System.out.println("Email: " + student.getEmail());
-//        System.out.println("Address: " + student.getAddress());
-//    }
-
-    
-    //=== List All Students ===
-    public void printStudentDetails() {
+    //=== 0.List Student ===
+    public void printStudentDetailsHeader() {
         System.out.println("\n=== STUDENT DETAILS ===");
-        System.out.println("\nStudent ID    Student Name    IC  Gender  Contact No  Email   Address");
-        System.out.println(  "=====================================================================");
-       // System.out.println(student.getStudId(),student.getStudName(),student.getIc(), student.getGender(), student.getContactNo(), student.getEmail(), )student.getAddress());
-//        return String.format("%-10s %-30s %-15s %10 %-15s %-30s %-50s",
-//                studId, studName, ic, gender, contactNo, email, address);
+        System.out.println("\n------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Student ID      Student Name              IC              Gender     Contact No      Email                     Address        ");
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------");
+    }
+
+    public void printStudentDetails(String studId, String studName, String ic, String gender, String contactNo, String email, String address) {
+        // Print the formatted output
+        System.out.printf("%-15s %-25s %-15s %-10s %-15s %-25s %-30s\n", studId, studName, ic, gender, contactNo, email, address);
+        System.out.println("");
+    }
+
+    //=== 1.Add Student ===
+    public void printAddStudentHeader() {
+        System.out.println("\nAdd New Student");
+        System.out.println("===============");
+    }
+
+    //=== 2.Remove Student === for new ver
+    public void printRemoveStudentHeader() {
+        System.out.println("\nRemove Student");
+        System.out.println("==============");
+    }
+
+    //=== 3.Amend Student ===
+    public void printAmendStudentHeader() {
+        System.out.println("\nAmend Student");
+        System.out.println("=============");
+    }
+
+    //=== 4.Search Student for Registered Course ===
+    public void printSearchStudentHeader() {
+        System.out.println("\nSearch Student for Registered Courses");
+        System.out.println("=====================================");
+    }
+
+    public void printCourseDeatilsHeader() {
+        System.out.println("\n=== COURSE DETAILS ===");
+        System.out.println("\n------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Course Code  Course Name                            Class Type  Semester    Credit  Faculty Fees     Programme  Status");
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------");
+    }
+
+    //=== 5.Add Student to Courses ===
+    public void printAddStudentToCoursesHeader() {
+        System.out.println("\nAdd Student to Courses");
+        System.out.println("======================");
+    }
+
+    //=== 6.Remove Student from Course ===
+    public void printRemoveStudentFromCourseHeader() {
+        System.out.println("\nRemove Student from Course");
+        System.out.println("==========================");
+    }
+
+    //=== 8.Filters Students for Course Citeria ===
+    public void printFiltersStudentsForCourseCiteriaHeader() {
+        System.out.println("\nFilters Students for Course Citeria");
+        System.out.println("===================================");
+    }
+
+    public void printFilterStudentHeader() {
+        //System.out.println("\n=== COURSE DETAILS ===");
+        System.out.println("------------------------------------------");
+        System.out.println("Student ID      Student Name              ");
+        System.out.println("------------------------------------------");
+    }
+
+    //=== 9.Summary Report 1 ===
+    public void printReportHeader1() {
+        System.out.println("\n=====================================================================================================================================");
+        System.out.println("                               TUNKU ABDUL RAHMAN UNIVERSITY OF MANAGEMENT AND TECHNOLOGY");
+        System.out.println("                                      STUDENT REGISTRATION MANAGEMENT SUBSYSTEM");
+        System.out.println("\n                                       STUDENT AGE ANALYSIS SUMMARY REPORT");
+        System.out.println(  "                                       -----------------------------------");
+        DateTime.currentDateTime();
+
+
+        
     }
     
-    //=== Add Student ===
-    public Student inputStudentDetails() {
-        System.out.println("\nAdd New Student");
-        System.out.println(  "===============");
-        String studentID = inputStudentID();
-        String studentName = inputStudentName();
-        String ic = inputIc();
-        String gender = inputGender();
-        String contactNo = inputContactNo();
-        String email = inputEmail();
-        String address = inputAddress();
-
-       // movieList.add("Inception");
-        
-        
-        System.out.println();
-        //return new Student();
-        return new Student(studentID, studentName, ic, gender, contactNo, email, address);
-    }  
-    public String inputStudentID() {
-        System.out.print("Enter Student ID [eg. 21WMR12345]: ");
-        String studID = scanner.nextLine();
-        return studID;
+    //=== 9.Summary Report 2 ===
+    public void printReportHeader2() {
+        System.out.println("\n=====================================================================================================================================");
+        System.out.println("                               TUNKU ABDUL RAHMAN UNIVERSITY OF MANAGEMENT AND TECHNOLOGY");
+        System.out.println("                                      STUDENT REGISTRATION MANAGEMENT SUBSYSTEM");
+        System.out.println("\n                                               STUDENT SUMMARY REPORT");
+        System.out.println("                                            --------------------------------");
+        DateTime.currentDateTime();
+        System.out.println("\nStudent ID        Student Name     IC     Gender  Contact No  Email   Address");
+        System.out.println("----------        ------------     --     ------  ----------  -----   --------");
+        System.out.println("\nTotal " + "gettotal" + "Students: ???");
+        System.out.println("\n-------------------------------------------------------------------------------------------------------------------------------------");
     }
 
-    public String inputStudentName() {
-        System.out.print("Enter Student Name: ");
-        String studName = scanner.nextLine();
-        return studName;
-    }
-
-    public String inputIc() {
-        System.out.print("Enter IC [eg. 123456-12-1234]: ");
-        String ic = scanner.nextLine();
-        return ic;
-    }
-
-    public String inputGender() {
-        System.out.print("Enter Gender [eg. Y/N]: ");
-        String gender = scanner.nextLine();
-        return gender;
-    }
-
-    public String inputContactNo() {
-        System.out.print("Enter Contact No [eg. 012-3456789]: ");
-         String contactNo = scanner.nextLine();
-        return contactNo;
-    }
-
-    public String inputEmail() {
-        System.out.print("Enter Email [eg. lisa@gmail.com]: ");
-        String email = scanner.nextLine();
-        return email;
-    }
-
-    public String inputAddress() {
-        System.out.print("Enter Address: ");
-        String address = scanner.nextLine();
-        return address;
+    public void printReportFooter() {
+        System.out.println("\n                                          END OF THE STUDENT SUMMARY REPORT");
+        System.out.println("=====================================================================================================================================");
     }
 }
